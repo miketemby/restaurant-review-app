dc5e976e15e7a5980bf0a03a0b5dac79659cfc5c
miketemby <mike@digitalserviceslab.com.au>
2018-11-22 19:33:46 +1100

    refactor: Updating SW fetch event to handle new requests
    Added logic to validate and cache requests that are not cached already

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
+// on install add assets to cache
 self.addEventListener('install', function(evt) {
     var urlsToCache = [
         '/',
@@ -20,11 +21,12 @@ self.addEventListener('install', function(evt) {
         '/js/restaurant_info.js',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
         'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
+        // how to add map tiles?
     ];
 
     evt.waitUntil(
        
-        caches.open('urls').then(function(cache) {
+        caches.open('assets').then(function(cache) {
             cache.addAll(urlsToCache);
         })
         
@@ -32,12 +34,37 @@ self.addEventListener('install', function(evt) {
 
 });
 
+// capture fetch events
 self.addEventListener('fetch', function(evt) {
     
+    //call respond with and return cached assets if they match
     evt.respondWith(
         caches.match(evt.request).then(function(response) {
-            if(response) return response;
-            return fetch(evt.request);
+            if(response) {
+                return response;
+            }
+
+            // if they dont match we want to try and fetch the live asset and capture and cache the response
+            // in both the request a response we need to clone it as they can only be consumed once and we dont want to stop the browswer getting the response. 
+            var fetchRequest = evt.request.clone();
+  
+            return fetch(fetchRequest).then(function(response) {
+
+                // Check if we received a valid response and if not just return the response becuase there is nothing to cache
+                if(!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+  
+                // if we get a valid response, clone it, cache it and return it
+                var responseToCache = response.clone();
+  
+                caches.open('assets').then(function(cache) {
+                    cache.put(evt.request, responseToCache);
+                });
+  
+                return response;
+            });
+
         })
     );
 
